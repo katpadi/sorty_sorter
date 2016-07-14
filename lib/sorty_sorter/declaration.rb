df2ae32759533f2d70f2b4719f75a83ed403a332
Kat Padilla <hello@katpadi.ph>
2016-07-14 19:23:44 +0800

    Remove unnecessary because already using activesuppport concern

--- lib/sorty_sorter/declaration.rb
+++ lib/sorty_sorter/declaration.rb
@@ -1,10 +1,6 @@
 module SortySorter
   module Declaration
     extend ActiveSupport::Concern
-    
-    def self.included(base)
-      base.extend ClassMethods
-    end
 
     module ClassMethods
       attr_reader :sort_columns
